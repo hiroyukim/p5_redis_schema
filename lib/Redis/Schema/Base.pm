82d6d1c8329698d4d5313056cfc0b0fb72ac66d3
hiroyuki <hiroyuki@norn.niflheimr.org>
2011-07-13 11:26:55 +0900

    メソッドを足した
    FIXME: 向こうが自動でやっている以上こちらも自動生成すべきだな

--- lib/Redis/Schema/Base.pm
+++ lib/Redis/Schema/Base.pm
@@ -19,4 +19,21 @@ sub hget {
     $self->SUPER::hget($self->schema_name,$key);
 }
 
+sub hsetnx {
+    my ($self,$key) = @_;
+    $self->SUPER::hsetnx($self->schema_name,$key);
+}
+
+sub hmset {
+    my $self    = shift;
+    my @f_v  = @_;
+    $self->SUPER::hmset($self->schema_name,@f_v);
+}
+
+sub hmget {
+    my $self = shift;
+    my @fields  = @_;
+    $self->SUPER::hmset($self->schema_name,@fields);
+}
+
 1;
